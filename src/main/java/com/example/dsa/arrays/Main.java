811a0b945d5aaea2d5684301c0b0b46f504d6532
Ali Katırcıo <ali.katircio@gmail.com>
2024-05-29 21:31:54 +0300

    added traverse, search and delete method for SingleDimensionArray

--- src/main/java/com/example/dsa/arrays/Main.java
+++ src/main/java/com/example/dsa/arrays/Main.java
@@ -10,5 +10,16 @@ public class Main {
         sda.insert(2,20);
         sda.insert(1,30);
         sda.insert(12,120);
+
+        var firstElement = sda.arr[0]; //O(1)
+        System.out.println(firstElement);
+
+        System.out.println("Array traversal");
+        sda.traverseArray();
+        sda.searchInArray(20);
+        sda.searchInArray(40);
+        sda.deleteValue(0);
+        System.out.println(sda.arr[0]);
+
     }
 }
--- src/main/java/com/example/dsa/arrays/SingleDimensionArray.java
+++ src/main/java/com/example/dsa/arrays/SingleDimensionArray.java
@@ -13,6 +13,13 @@ public class SingleDimensionArray {
          }
      }
 
+    /**
+     * Insert value an array
+     * Time Complexity O(1)
+     * Space Complexity O(n)
+     * @param location array index
+     * @param value array value
+     */
      public void insert(int location, int value) { //Time complexity O(1)
          try {
 
@@ -26,4 +33,63 @@ public class SingleDimensionArray {
              System.out.println("Invalid index to access array!"); //O(1)
          }
      }
+
+     //
+
+    /**
+     * Array Traversal
+     * Time Complexity O(n)
+     * Space Complexity O(1)
+     */
+    public void traverseArray() {
+
+         try {
+
+             for (int i = 0; i < arr.length; i++) {
+
+                 System.out.print(arr[i] + " ");
+             }
+             System.out.println("\n");
+         }catch (Exception e) {
+             System.out.println("Array no longer exists!");
+         }
+    }
+
+    //Search for an element in the given array
+
+    /**
+     * Search for an element in the given array
+     * Time Complexity O(n)
+     * Space Complexity O(1)
+     * @param value searchValue
+     */
+    public void searchInArray(int value) {
+
+        for(int i : arr){ //O(N)
+
+            if (i == value){ //O(1)
+
+                System.out.println("Value is found "+ i); //O(1)
+                return; //O(1)
+            }
+        }
+        System.out.println(value + " is not found"); //O(1)
+    }
+
+    /**
+     * Delete value from array
+     * Time Complexity O(1)
+     * Space Complexity O(1)
+     * @param valueIndex delete index
+     */
+    public void deleteValue(int valueIndex) {
+
+        try {
+
+            arr[valueIndex] = Integer.MIN_VALUE; // O(1)
+            System.out.println("The value has been deleted successfully!"); // O(1)
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("The value that is provided is not in the range of array!"); // O(1)
+        }
+    }
 }
