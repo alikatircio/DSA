c9e39d975c6fcc8c8841902b36dde54c8d566016
Ali Katırcıo <ali.katircio@gmail.com>
2024-05-30 13:57:03 +0300

    added RemoveDublicates class for solution of leetcode 26

---
+++ src/main/java/com/example/dsa/arrays/RemoveDuplicates.java
@@ -0,0 +1,29 @@
+package com.example.dsa.arrays;
+
+import java.util.Arrays;
+
+public class RemoveDuplicates {
+
+    public static int removeDuplicates(int[] nums) {
+
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int i = 0;
+
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2};
+        System.out.println(removeDuplicates(arr));
+    }
+}
