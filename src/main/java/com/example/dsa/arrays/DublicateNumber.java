23ac0108164fa74f89a5988eb51f6cb3126bd914
Ali Katırcıo <ali.katircio@gmail.com>
2024-05-30 13:32:45 +0300

    added remove dublicate values with new array

---
+++ src/main/java/com/example/dsa/arrays/DublicateNumber.java
@@ -0,0 +1,34 @@
+package com.example.dsa.arrays;
+
+import java.util.Arrays;
+
+public class DublicateNumber {
+
+    public static int[] removeDuplicates(int[] array) {
+
+        int n = array.length;
+        int[] uniqueArray = new int[n];
+        int index = 0;
+
+        for (int i = 0; i < n; i++) {
+            boolean isDuplicate = false;
+            for (int j = i + 1; j < n; j++) {
+                if (array[i] == array[j]) {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            if (!isDuplicate) {
+                uniqueArray[index++] = array[i];
+            }
+        }
+
+        return Arrays.copyOf(uniqueArray, index);
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(removeDuplicates(arr)));
+    }
+}
